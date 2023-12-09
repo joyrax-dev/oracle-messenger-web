2af346ce1a4afd4670c4153483ba60c55b3265e5
joyrax-dev <joyrax.dev@gmail.com>
2023-12-10 00:59:08 +1000

    add funcs reauth tokens

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-    <nav>
+    <div class="flex flex-row justify-center content-center gap-4">
         <router-link to="/">Home</router-link> |
         <router-link to="/login">Login</router-link> |
         <router-link to="/register">Register</router-link> |
         <router-link to="/about">About</router-link>
-    </nav>
+    </div>
     <router-view/>
 </template>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,28 @@
+import createPersistedState from 'vuex-persistedstate'
 import { createStore } from 'vuex'
 import socket from '@/socket'
 
 export default createStore({
 	state: {
         is_authenticate: false,
+        reauthentication_token: null
 	},
 	getters: {
         IS_CONNECTION: state => socket.connected,
         IS_AUTHENTICATE: state => state.is_authenticate,
+        IS_REAUTHENTICATE: state => state.reauthentication_token == null ? false : true,
+        GET_REAUTHENTICATION_TOKEN: state => state.reauthentication_token
 	},
 	mutations: {
-        SET_AUTHENTICATE: (state, payload) => state.is_authenticate = payload
+        SET_AUTHENTICATE: (state, payload) => state.is_authenticate = payload,
+        SET_REAUTHENTICATION_TOKEN: (state, payload) => state.reauthentication_token = payload
 	},
 	actions: {
 	},
 	modules: {
-	}
+	},
+    plugins: [createPersistedState({
+        paths: ['reauthentication_token'],
+        key: 'vuextest'
+    })]
 })
